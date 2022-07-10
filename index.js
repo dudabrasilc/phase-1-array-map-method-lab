7a8862b1409d290b1769602bd2e4d2128f70a4a1
Maria Coitinho <brasildu@gmail.com>
2022-07-10 16:51:33 -0500

    commit tree b49006b6a33c17b82bcaf2d4e5111d24a8a1af47

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() {
+          return tutorials.map(function (element) {
+          let separatedArray = element.split(" ");
+          // console.log(separatedArray);
+          let result = separatedArray.map((elem) => {
+            // console.log(elem)
+          return elem[0].toUpperCase() + elem.substring(1);
+          }
+          )
+          return result.join(' ');
+        }
+      )
+    }
+  
+
+console.log(titleCased())
+
+
+// const robots = [
+//   { name: "Johnny 5", modes: 5, isActivated: false },
+//   { name: "C3PO", modes: 3, isActivated: false },
+//   { name: "Sonny", modes: 2.5, isActivated: false },
+//   { name: "Baymax", modes: 1.5, isActivated: false },
+// ];
+
+// const activateRobots = robots.map((element) => {
+//   let newElement = {...element};
+//   newElement.isActivated = "true";
+//   // console.log(newElement)
+//   newElement.modes = newElement.modes * 2;
+//   // console.log(newElement)
+//   return newElement;
+// })
+
+// console.log(activateRobots)
+// console.log(robots)
